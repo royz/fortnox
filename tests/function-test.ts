906696893c164a6da8e772f3b1e77413b322980c
royz <rajorshi.me@gmail.com>
2026-04-01 11:47:26 +0530

    add support for output type and manage the error more reliably

--- tests/function-test.ts
+++ tests/function-test.ts
@@ -1,6 +1,8 @@
 import z from "zod";
 import { paths } from "./path-list";
-import type { ZodType } from "zod";
+import type { ZodError, ZodType } from "zod";
+import { env } from "../env";
+import { writeFile } from "node:fs/promises";
 
 type Paths = typeof paths;
 
@@ -14,8 +16,6 @@ type RequestOptions = {
   query?: Record<string, any>;
   params?: Record<string, any>;
   body?: any;
-  /** if true, skip zod validation of request parameters */
-  skipValidation?: boolean;
 }
 
 type FortnoxErrorRespose = {
@@ -26,120 +26,150 @@ type FortnoxErrorRespose = {
   }
 }
 
-type InitFortnoxOptions = { accessToken: string } | { proxyUrl: string; apiKey: string; tenantId: string; };
-
-function formatZodError(error: z.ZodError) {
-  const errors: Record<string, Record<string, string[]>> = {};
-
-  for (const issue of error.issues) {
-    const [root, ...path] = issue.path.map(String);
-    if (!root) continue;
-
-    if (!(root in errors)) {
-      errors[root] = {};
-    }
-
-    const pathKey = path.length > 0 ? path.join(".") : "_";
-    if (!(pathKey in errors[root]!)) {
-      errors[root]![pathKey] = [];
-    }
-    errors[root]![pathKey]!.push(issue.message);
+type ErrorRespose = {
+  "ErrorSource": "fortnox" | "validation" | "unknown",
+  "ErrorInformation": {
+    "error": number,
+    "message": string,
+    "code": number,
   }
-
-  return errors;
 }
 
+type InitFortnoxOptions =
+  | { accessToken: string; skipValidation?: boolean; }
+  | { proxyUrl: string; apiKey: string; tenantId: string; skipValidation?: boolean; };
 
-function validateRequestOptions(reqOptions: RequestOptions) {
-  if (reqOptions.skipValidation) {
-    return;
-  }
-
+function validateRequestOptions(reqOptions: RequestOptions): ZodError | undefined {
   const methods = (paths as any)[reqOptions.path];
 
-  const schemas = methods[reqOptions.method.toLowerCase()] as ZodType | undefined;
-  if (!schemas) return;
+  const schemas = methods[reqOptions.method.toLowerCase()]?.input as ZodType | undefined;
+  if (!schemas) return undefined;
 
   const { error } = schemas.safeParse({
     query: reqOptions.query,
     params: reqOptions.params,
     body: reqOptions.body,
   })
-
-  if (error) {
-    console.error("Validation failed:", formatZodError(error));
-    throw new Error("Request validation failed");
-  }
+  return error;
 }
 
 async function request(reqOptions: RequestOptions, initOptions: InitFortnoxOptions) {
-  validateRequestOptions(reqOptions);
+  try {
+    if (!initOptions.skipValidation) {
+      const error = validateRequestOptions(reqOptions);
+      if (error) {
+        return {
+          ErrorSource: "validation",
+          ErrorInformation: {
+            message: z.prettifyError(error),
+            error: -1,
+            code: -1,
+          }
+        } satisfies ErrorRespose;
+      }
+    }
 
-  let headers: Record<string, string>;
-  let baseUrl: string;
+    let headers: Record<string, string>;
+    let baseUrl: string;
 
-  if ("accessToken" in initOptions) {
-    headers = {
-      "Authorization": `Bearer ${initOptions.accessToken}`,
-    };
-    baseUrl = "https://api.fortnox.se";
-  } else {
-    headers = {
-      "x-api-key": initOptions.apiKey,
-      "x-tenant-id": initOptions.tenantId,
-    };
-    baseUrl = initOptions.proxyUrl;
-  }
+    if ("accessToken" in initOptions) {
+      headers = {
+        "Authorization": `Bearer ${initOptions.accessToken}`,
+      };
+      baseUrl = "https://api.fortnox.se";
+    } else {
+      headers = {
+        "x-api-key": initOptions.apiKey,
+        "x-tenant-id": initOptions.tenantId,
+      };
+      baseUrl = initOptions.proxyUrl;
+    }
 
-  const searchParams = new URLSearchParams();
-  if (reqOptions.query) {
-    Object.entries(reqOptions.query).forEach(([key, value]) => {
-      searchParams.append(key, String(value));
+    const searchParams = new URLSearchParams();
+    if (reqOptions.query) {
+      Object.entries(reqOptions.query).forEach(([key, value]) => {
+        searchParams.append(key, String(value));
+      });
+    }
+
+    let path = reqOptions.path;
+    if (reqOptions.params) {
+      Object.entries(reqOptions.params).forEach(([key, value]) => {
+        path = path.replace(`{${key}}`, encodeURIComponent(String(value)));
+      });
+    }
+
+    const url = `${baseUrl}${path}${searchParams.size > 0 ? `?${searchParams.toString()}` : ""}`;
+
+    console.log({
+      url,
+      method: reqOptions.method,
+      body: reqOptions.body,
+      headers,
     });
-  }
 
-  let path = reqOptions.path;
-  if (reqOptions.params) {
-    Object.entries(reqOptions.params).forEach(([key, value]) => {
-      path = path.replace(`{${key}}`, encodeURIComponent(String(value)));
+    const response = await fetch(url, {
+      method: reqOptions.method,
+      body: reqOptions.body ? JSON.stringify(reqOptions.body) : null,
+      headers,
     });
+
+    const resposneData = await response.json();
+
+    if (response.status < 300) { // Success
+      return { error: null, data: resposneData }
+    } else { // Error
+      return {
+        error: {
+          ErrorSource: "fortnox",
+          ErrorInformation: (resposneData as FortnoxErrorRespose).ErrorInformation,
+        } satisfies ErrorRespose,
+        data: null,
+      }
+    }
+  } catch (e: any) {
+    return {
+      ErrorSource: "unknown",
+      ErrorInformation: {
+        message: e.message,
+        error: -1,
+        code: -1,
+      }
+    } satisfies ErrorRespose;
   }
-
-  const url = `${baseUrl}${path}${searchParams.size > 0 ? `?${searchParams.toString()}` : ""}`;
-
-  console.log({
-    url,
-    method: reqOptions.method,
-    body: reqOptions.body,
-    headers,
-  });
-
-  return {} as any;
-  // return new Promise<Response>(resolve => resolve(new Response()));
 }
 
 type InferInput<T> = T extends { input: infer TInput extends ZodType } ? z.infer<TInput> : never;
 type InferOutput<T> = T extends { output: infer TOutput extends ZodType } ? z.infer<TOutput> : never;
 
+type FortnoxResult<TData> = Promise<{ error: ErrorRespose; data: null } | { error: null; data: TData }>;
+type FortnoxMethods<TPath extends keyof Paths> = {
+  [TMethod in keyof Paths[TPath]]: undefined extends InferInput<Paths[TPath][TMethod]>
+  ? (options?: InferInput<Paths[TPath][TMethod]>) => FortnoxResult<InferOutput<Paths[TPath][TMethod]>>
+  : (options: InferInput<Paths[TPath][TMethod]>) => FortnoxResult<InferOutput<Paths[TPath][TMethod]>>;
+};
+
 function initFortnox(initOptions: InitFortnoxOptions) {
-  return function fortnox<TPath extends keyof Paths>(path: TPath): { [TMethod in keyof Paths[TPath]]: (options: InferInput<Paths[TPath][TMethod]>) => Promise<{ error: FortnoxErrorRespose, data: null } | { error: null, data: InferOutput<Paths[TPath][TMethod]> }> } {
+  return function fortnox<TPath extends keyof Paths>(path: TPath): FortnoxMethods<TPath> {
     return Object.fromEntries(
       METHODS.map(method => [
         method.toLowerCase(),
         (options: any) => request({ path, method, ...options }, initOptions),
       ])
-    ) as unknown as { [TMethod in keyof Paths[TPath]]: (options: InferInput<Paths[TPath][TMethod]>) => Promise<{ error: FortnoxErrorRespose, data: null } | { error: null, data: InferOutput<Paths[TPath][TMethod]> }> };
+    ) as any;
   }
 }
 
-
-const fx = initFortnox({ accessToken: 'text-access-token' });
-const fx2 = initFortnox({ proxyUrl: "https://test.test", apiKey: "xxyyxx", tenantId: "12345678" });
-
-const res = await fx("/3/invoices/{DocumentNumber}").get({
-  params: { DocumentNumber: "123" },
+// const fx = initFortnox({ accessToken: 'text-access-token' });
+const fx = initFortnox({
+  proxyUrl: env.PROXY_URL,
+  apiKey: env.API_KEY,
+  tenantId: env.TENANT_ID
 });
 
-if (res.error) throw "";
+const res = await fx("/3/invoices").get()
 
-res.data;
+if (res.error) throw res.error;
+
+await writeFile("response.json", JSON.stringify(res.data, null, 2), "utf-8");
+console.log(res.data)
