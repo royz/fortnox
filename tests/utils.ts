653e13c6f7ff5c7fed5ace02c579535cc23405e0
royz <rajorshi.me@gmail.com>
2026-04-01 19:11:44 +0530

    read the original spec after verifying hash

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,12 +1,35 @@
 import { readFile } from "node:fs/promises";
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
 import path from "node:path";
 import type { OpenAPIV3 } from "openapi-types";
 
-export async function getSpecFromFile(type: "original" | "patched"): Promise<OpenAPIV3.Document> {
+const SPEC_DIR = path.join(import.meta.dirname, "../spec");
+
+/** Get the OpenAPI specification (original or patched) from a file 
+ * @param type - "original" to get the original spec, "patched" to get the patched spec
+ * @param bypassHashCheck - If true, will skip the hash check for the original spec file (useful for testing with modified spec files)
+ * @returns The OpenAPI specification as a OpenAPIV3.Document object
+*/
+export async function getSpecFromFile(type: "original" | "patched", bypassHashCheck?: boolean): Promise<OpenAPIV3.Document> {
   const fileName = type === "original" ? "openapi.json" : "openapi-patched.json";
-  return JSON.parse(await readFile(path.join(import.meta.dirname, "../spec", fileName), "utf-8"));
+  const filePath = path.join(SPEC_DIR, fileName);
+
+  if (type === "original" && !bypassHashCheck) {
+    const fileHash = await getFileHash(filePath);
+    const hashPath = path.join(SPEC_DIR, "openapi.hash");
+
+    const expectedHash = (await readFile(hashPath, "utf-8")).trim().split("\n")?.at(-1);
+    if (fileHash !== expectedHash) {
+      throw new Error(`Original spec file hash mismatch! Expected hash: ${expectedHash}, got ${fileHash}. Re-download the spec using "pnpm run download".`);
+    }
+  }
+  return JSON.parse(await readFile(path.join(SPEC_DIR, fileName), "utf-8"));
 }
 
+/** Derive the equivalent zod type from an OpenAPI schema object 
+ * @returns A string representing the zod type (e.g. `"z.string()"`, `"z.number().optional()"`, `"z.enum(['a', 'b'])"`, etc.)
+*/
 function getZodTypeForParameter(schema: OpenAPIV3.SchemaObject, required: boolean) {
   let t = "z.any()";
 
@@ -98,4 +121,20 @@ export function getParamsAsZodObject(parameters: OpenAPIV3.ParameterObject[]) {
     queryParams: count.query.total > 0 ? queryParams : null,
     fullyOptional: count.path.total === 0 && count.query.required === 0
   };
+}
+
+
+export async function getFileHash(filePath: string) {
+  return await new Promise<string>((resolve, reject) => {
+    const hash = createHash("sha256");
+    const stream = createReadStream(filePath);
+
+    stream.on("error", reject);
+    stream.on("data", (chunk: Buffer) => {
+      hash.update(chunk);
+    });
+    stream.on("end", () => {
+      resolve(hash.digest("hex"));
+    });
+  });
 }
