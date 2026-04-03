af3dff2f083f92f51e2bafcc2801ad7b92d4b0f7
royz <rajorshi.me@gmail.com>
2026-04-03 19:32:35 +0530

    wrap the code in a function

--- src/lib/official-schema-generator.ts
+++ src/lib/official-schema-generator.ts
@@ -3,11 +3,18 @@ import path from "node:path";
 import { SchemasParser } from "./parser";
 import { getSpecFromFile } from "./utils";
 
-const TYPES_DIR = path.join(import.meta.dirname, "../types");
-const outputPath = path.join(TYPES_DIR, "official-schemas.ts");
 
-const spec = await getSpecFromFile("original");
-const parser = new SchemasParser(spec);
-const schemaString = await parser.parse();
+export async function generateOfficialSchemas() {
+  const TYPES_DIR = path.join(import.meta.dirname, "../types");
+  const outputPath = path.join(TYPES_DIR, "official-schemas.ts");
 
-await writeFile(outputPath, schemaString);
+  const spec = await getSpecFromFile("original");
+  const parser = new SchemasParser(spec);
+  const schemaString = await parser.parse();
+
+  await writeFile(outputPath, schemaString);
+}
+
+if (import.meta.filename === process.argv[1]) {
+  await generateOfficialSchemas();
+}
