8765adbfaa43bf80f7711764c52d22136fa0b138
royz <rajorshi.me@gmail.com>
2026-04-08 10:56:11 +0530

    export MetaInformation type

--- src/types/custom/index.ts
+++ src/types/custom/index.ts
@@ -1,2 +1,9 @@
 export type * from "./invoice.type";
 export type * from "./voucher.type";
+
+/** Pagination information */
+export type MetaInformation = {
+  "@CurrentPage": number;
+  "@TotalPages": number;
+  "@TotalResources": number;
+}
--- src/types/utility-types.ts
+++ src/types/utility-types.ts
@@ -1,37 +1,32 @@
+import type { MetaInformation } from "./custom";
+
 /** Flatten composite types to make it more readable */
 export type Prettify<T> = {
-	[K in keyof T]: T[K];
+  [K in keyof T]: T[K];
 };
 
 /** Make specific properties of an object required, all others optional */
 export type PartialRequired<Type, Key extends keyof Type> = Prettify<
-	Partial<Type> & {
-		[K in Key]-?: Type[K];
-	}
+  Partial<Type> & {
+    [K in Key]-?: Type[K];
+  }
 >;
 
 /** Make all properties of an object required */
 export type AllRequired<T> = {
-	[K in keyof T]-?: T[K];
+  [K in keyof T]-?: T[K];
 };
 
 /** Wraps a list of items with pagination information */
 export type FortnoxListWrap<TKey extends string, TItem> = Prettify<
-	{
-		/** Pagination information */
-		MetaInformation: {
-			/** Format: int32 */
-			"@CurrentPage": number;
-			/** Format: int32 */
-			"@TotalPages": number;
-			/** Format: int32 */
-			"@TotalResources": number;
-		};
-	} & {
-		[K in TKey]: TItem[];
-	}
+  {
+    /** Pagination information */
+    MetaInformation: MetaInformation;
+  } & {
+    [K in TKey]: TItem[];
+  }
 >;
 
 export type OmitNever<T> = {
-	[K in keyof T as T[K] extends never ? never : K]: T[K];
+  [K in keyof T as T[K] extends never ? never : K]: T[K];
 };
